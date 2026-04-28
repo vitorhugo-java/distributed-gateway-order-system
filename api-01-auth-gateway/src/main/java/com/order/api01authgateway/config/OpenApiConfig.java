73ffe2c242f880e943e73215b6c27a7b06a2cbfd
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-04-28 13:01:59 -0300

    feat: add swagger configuration

---
+++ api-01-auth-gateway/src/main/java/com/order/api01authgateway/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.order.api01authgateway.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Auth Gateway API").version("1.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
