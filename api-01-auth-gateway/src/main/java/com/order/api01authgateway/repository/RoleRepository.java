7e0b20e439bf6c102cf8c2599630f61c5ce30205
vitorhugo-java <vitorhugoalvesferreira@gmail.com>
2026-04-28 19:45:40 -0300

    refractor: Add @Repository annotation to repositories
    Import org.springframework.stereotype.Repository and annotate RoleRepository and UserRepository with @Repository. This makes the repository interfaces explicit Spring stereotypes (aids component scanning and exception translation clarity). Files changed: RoleRepository.java, UserRepository.java.

--- api-01-auth-gateway/src/main/java/com/order/api01authgateway/repository/RoleRepository.java
+++ api-01-auth-gateway/src/main/java/com/order/api01authgateway/repository/RoleRepository.java
@@ -2,8 +2,11 @@ package com.order.api01authgateway.repository;
 
 import com.order.api01authgateway.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findByName(String name);
 }
--- api-01-auth-gateway/src/main/java/com/order/api01authgateway/repository/UserRepository.java
+++ api-01-auth-gateway/src/main/java/com/order/api01authgateway/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.order.api01authgateway.repository;
 
 import com.order.api01authgateway.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
