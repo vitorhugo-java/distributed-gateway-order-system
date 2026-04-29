534f41f3b8dd295ff8ff686228c6faab12482680
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-04-29 11:41:06 +0000

    feat: Enhance CI/CD workflow and add documentation deployment

--- api-01-auth-gateway/src/test/java/com/order/api01authgateway/Api01AuthGatewayApplicationTests.java
+++ api-01-auth-gateway/src/test/java/com/order/api01authgateway/Api01AuthGatewayApplicationTests.java
@@ -2,8 +2,10 @@ package com.order.api01authgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Api01AuthGatewayApplicationTests {
 
     @Test
--- api-02-orders-crud/src/test/java/com/order/api02orderscrud/Api02OrdersCrudApplicationTests.java
+++ api-02-orders-crud/src/test/java/com/order/api02orderscrud/Api02OrdersCrudApplicationTests.java
@@ -2,8 +2,10 @@ package com.order.api02orderscrud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Api02OrdersCrudApplicationTests {
 
     @Test
